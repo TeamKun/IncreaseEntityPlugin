2842f1910e4d83d3ec60dd53641477e99ffd7039
Yu212 <41502287+Yu212@users.noreply.github.com>
2021-07-20 00:37:40 +0900

    Initial commit

---
+++ src/main/java/net/kunmc/lab/increaseentity/IncreaseEntityPlugin.java
@@ -0,0 +1,13 @@
+package net.kunmc.lab.increaseentity;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class IncreaseEntityPlugin extends JavaPlugin {
+    @Override
+    public void onEnable() {
+    }
+
+    @Override
+    public void onDisable() {
+    }
+}
